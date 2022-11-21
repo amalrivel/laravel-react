4643338bba562a94a1f0c538d13cb553b00afecb
amalrivel <amalrivel@gmail.com>
2022-11-22 00:54:33 +0700

    adding prettier

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,32 +1,41 @@
-import React, { forwardRef, useEffect, useRef } from 'react';
+import React, { forwardRef, useEffect, useRef } from "react";
 
 export default forwardRef(function TextInput(
-    { type = 'text', name, value, className, autoComplete, required, isFocused, handleChange },
-    ref
+  {
+    type = "text",
+    name,
+    value,
+    className,
+    autoComplete,
+    required,
+    isFocused,
+    handleChange,
+  },
+  ref
 ) {
-    const input = ref ? ref : useRef();
+  const input = ref ? ref : useRef();
 
-    useEffect(() => {
-        if (isFocused) {
-            input.current.focus();
+  useEffect(() => {
+    if (isFocused) {
+      input.current.focus();
+    }
+  }, []);
+
+  return (
+    <div className="flex flex-col items-start">
+      <input
+        type={type}
+        name={name}
+        value={value}
+        className={
+          `border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ` +
+          className
         }
-    }, []);
-
-    return (
-        <div className="flex flex-col items-start">
-            <input
-                type={type}
-                name={name}
-                value={value}
-                className={
-                    `border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ` +
-                    className
-                }
-                ref={input}
-                autoComplete={autoComplete}
-                required={required}
-                onChange={(e) => handleChange(e)}
-            />
-        </div>
-    );
+        ref={input}
+        autoComplete={autoComplete}
+        required={required}
+        onChange={(e) => handleChange(e)}
+      />
+    </div>
+  );
 });
